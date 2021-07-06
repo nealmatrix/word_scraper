c4119f4cdbe2b5a23391c53485bd0840f2bd02e4
nealmatrix <nealmatrixgg@gmail.com>
2021-07-06 17:40:20 +0800

    create new table to change the id sequence

--- words.sql
+++ words.sql
@@ -1,3 +1,4 @@
+/* Create table words */
 --CREATE TABLE--
 CREATE TABLE WORDS (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -15,12 +16,35 @@ SELECT * FROM WORDS;
 SELECT * FROM WORDS WHERE id > 7;
 
 --DELETE ALL THE RECORDS
---DELETE FROM WORDS;
+--DELETE FROM WORDS2;
 
 --DELETE THE SPECIFIC RECORD
 --DELETE FROM WORDS WHERE id = 8;
 
---TEST--
+/* Change id */
+CREATE TABLE WORDS2 (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    word varchar(255),
+    pron_us varchar(255),
+    collins_meaning varchar(255),
+    collins_example varchar(255),
+    episode varchar(255)
+);
+
+--INSERT
+INSERT INTO WORDS2(word, pron_us, collins_meaning, collins_example, episode)
+SELECT word, pron_us, collins_meaning, collins_example, episode
+FROM WORDS;
+
+
+
+--DROP
+--DROP TABLE WORDS;
+
+--RENAME
+--ALTER TABLE WORDS2 RENAME TO WORDS;
+
+/* For test functions */
 --CREATE TEST TABLE--
 CREATE TABLE TEST (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -44,4 +68,5 @@ INSERT INTO TEST (collins_meaning) VALUES ('"''"');
 --ALTER TABLE TEST DROP id;
 --ALTER TABLE TEST ADD id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT FIRST;
 
+--RENAME
 ALTER TABLE TEST RENAME TO TEST1;
